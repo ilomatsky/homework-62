0e6be29d88c45ddc6b38214f159ea2899ba1e7ca
Ilya Lomatsky <ilomatsky@gmail.com>
2023-11-27 13:04:08 +0600

    The About block has been completed

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,8 +1,10 @@
 const About = () => {
   return (
-    <div>
-      <h2>About Us</h2>
-      <p>Learn more about our company and our team.</p>
+    <div className="about-title">
+      <div className="about-text">
+        <h2>About Us</h2>
+        <p>Learn more about our company and our team</p>
+      </div>
     </div>
   );
 };
